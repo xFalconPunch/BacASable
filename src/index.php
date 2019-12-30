8af92c7d514a0aa28f88c744ed1d8a932445c3f3
Franck <falcou.franck@gmail.com>
2019-12-30 20:50:45 +0100

    start project

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bacasable</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  
+  <!-- Bootstrap core CSS -->
+  <link href="framework/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+  <?php include "./include/header.html" ?>
+  <app-root>
+  
+  
+  
+  </app-root>
+</body>
+</html>
